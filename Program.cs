6a9ef2e2a86be7fec551bacca635b8c7eff17dee
Vinicius Otacilio <viniotacilio@gmail.com>
2021-05-24 21:10:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomPi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rd = new Random();
+            int acertos = 0;
+            int n = 5000000;
+
+                for (int i = 0; i < n; i++)
+                {
+                    double x = rd.NextDouble();
+                    double y = rd.NextDouble();
+
+                    if (x * x + y * y < 1)
+                    {
+                        acertos++;
+                    }
+                }
+
+            double result = (4.0 * acertos / n);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
